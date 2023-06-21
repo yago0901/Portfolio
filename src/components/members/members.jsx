61d35dac1a758e8a5134130eb45c4295353f3849
Yago Santos Gigeck <yago0901@gmail.com>
2023-06-21 11:46:59 -0300

    feat: added experiences from Fuvir Atender and Parafuzo

--- src/components/members/members.jsx
+++ src/components/members/members.jsx
@@ -4,6 +4,67 @@ const Members = () => {
   return (
     <div id="members" className="container members-container">
       <h1 className="member-txt">Projetos</h1>
+
+      <div className="member member-2">
+        <div className="member-img6"></div>
+        <div className="member-info">
+          <h1 className="name">Parafuzo</h1>
+          <h3 className="position">Front-end</h3>
+          <h4 className="about">
+            Desenvolvendo novas funcionalidades vindas de QA. Aplicando
+            melhorias e mantendo código atualizado. Metodologia Scrum e
+            linguagem React Js.
+          </h4>
+          <a
+            href="https://parafuzo.com/"
+            target="_blank"
+            className="contact-member"
+            rel="noreferrer"
+          >
+            <span>Acessar site</span>
+          </a>
+        </div>
+      </div>
+
+      <div className="member member-2">
+        <div className="member-img8"></div>
+        <div className="member-info">
+          <h1 className="name">Atender Digital</h1>
+          <h3 className="position">Desenvolvedor Laravel</h3>
+          <h4 className="about">
+            Desenvolvimento e manutenção do site e funcionalidades. PhP Laravel.
+          </h4>
+          <a
+            href="https://atender.digital/"
+            target="_blank"
+            className="contact-member"
+            rel="noreferrer"
+          >
+            <span>Acessar site</span>
+          </a>
+        </div>
+      </div>
+
+      <div className="member member-2">
+        <div className="member-img7"></div>
+        <div className="member-info">
+          <h1 className="name">Fuvir</h1>
+          <h3 className="position">Desenvolvedor</h3>
+          <h4 className="about">
+            Criação do projeto de investimento e correções na na plataforma.
+            Além de criação de imagens, vídeos entre outros.
+          </h4>
+          <a
+            href="https://parafuzo.com/"
+            target="_blank"
+            className="contact-member"
+            rel="noreferrer"
+          >
+            <span>Acessar site</span>
+          </a>
+        </div>
+      </div>
+
       <div className="member member-1">
         <div className="member-img"></div>
         <div className="member-info">
@@ -20,27 +81,6 @@ const Members = () => {
             rel="noreferrer"
           >
             <span>Acessar site</span>
-          </a>
-        </div>
-      </div>
-
-      <div className="member member-2">
-        <div className="member-img"></div>
-        <div className="member-info">
-          <h1 className="name">Parafuzo</h1>
-          <h3 className="position">Front-end</h3>
-          <h4 className="about">
-            Desenvolvendo novas funcionalidades vindas de QA. Aplicando
-            melhorias e mantendo código atualizado. Metodologia Scrum e
-            linguagem React Js.
-          </h4>
-          <a
-            href="https://parafuzo.com/"
-            target="_blank"
-            className="contact-member"
-            rel="noreferrer"
-          >
-            <span>contact</span>
           </a>
         </div>
       </div>
